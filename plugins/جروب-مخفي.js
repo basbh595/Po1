ad89205986f09db6cd87332a34646b499e52f5ed
basbh595 <basbh2088@gmail.com>
2025-01-31 20:32:00 +0300

    Update جروب-مخفي.js

--- plugins/جروب-مخفي.js
+++ plugins/جروب-مخفي.js
@@ -1,26 +1,15 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async(m, { conn, text, participants }) => {
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+
+let handler = async (m, { conn, text, participants }) => {
+    let users = participants.map(u => conn.decodeJid(u.id))
+    let msg = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: text || '' } }, { userJid: conn.user.id, mentions: users })
+    await conn.relayMessage(m.chat, msg.message, {})
 }
 
-    conn.sendMessage(m.chat, { text: text, mentions: participants.map(a => a.id) }, {quoted:fkontak})
-    }
 handler.help = ['hidetag']
 handler.tags = ['group']
-handler.command = /^(hidetag|مخفي)$/i
+handler.command = /^(خفي|مهم|@)$/i
 handler.group = true
-handler.admin = true
+handler.owner = true
+
 export default handler
